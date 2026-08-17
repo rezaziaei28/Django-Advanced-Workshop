a63d648452e782ac94e43403f6d81fec574f021c
zia ziaei <rezaziaei28@gmail.com>
2026-08-17 19:07:23 +0330

    feat(static): configure static and media file serving
    - Add static() URL patterns for media and static files in development
    - Set STATIC_ROOT and STATIC_URL for production collection
    - Configure MEDIA_ROOT and MEDIA_URL for user-uploaded files
    - Create STATICFILES_DIRS for additional static file directories
    - Conditionally serve static/media files only when DEBUG is True
    
    This enables proper file serving for development and prepares
    the project for production static file collection with collectstatic.

--- core/core/settings.py
+++ core/core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,7 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfile'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
