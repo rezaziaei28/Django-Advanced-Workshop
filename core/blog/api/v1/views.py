61f1f7b6b344e907d5694f060a3c62426da29e82
zia ziaei <rezaziaei28@gmail.com>
2026-09-05 21:26:57 +0330

    use try and except in view for show post in the page and use status for show 404(not found) page

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializers
 from ...models import Post
+from rest_framework import status
+
 # Create your views here.
 
 data = {
@@ -16,6 +18,10 @@ def post_list(request):
 
 @api_view()
 def post_detail(request,id):
-      post = Post.objects.get(pk=id)
-      serializer = PostSerializers(post)
-      return Response(serializer.data)
+      try:
+            post = Post.objects.get(pk=id)
+            serializer = PostSerializers(post)
+            return Response(serializer.data)
+      except Post.DoesNotExist:
+            return Response({"detail":"Post is not exist"}, status=status.HTTP_404_NOT_FOUND)
+      
