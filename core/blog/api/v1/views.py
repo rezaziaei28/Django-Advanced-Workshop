a0b4aecfc13c2ee6079ee8cbda57e5c3f196d3ae
zia ziaei <rezaziaei28@gmail.com>
2026-09-06 17:22:56 +0330

    replace get_object_or_404 with try and except in views at post_detail and remove data from views

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,25 +3,27 @@ from rest_framework.response import Response
 from .serializers import PostSerializers
 from ...models import Post
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-
-data = {
-      "id": 1,
-      "name": "Zia",
-      "job": "backend developer",
-}
 
 @api_view()
 def post_list(request):
       return Response("ok")
 
+
 @api_view()
 def post_detail(request,id):
-      try:
-            post = Post.objects.get(pk=id)
-            serializer = PostSerializers(post)
-            return Response(serializer.data)
-      except Post.DoesNotExist:
-            return Response({"detail":"Post is not exist"}, status=status.HTTP_404_NOT_FOUND)
-      
+      post = get_object_or_404(Post, pk=id)
+      post = Post.objects.get(pk=id)
+      serializer = PostSerializers(post)
+      return Response(serializer.data)
+
+      # try:
+      #       post = Post.objects.get(pk=id)
+      #       serializer = PostSerializers(post)
+      #       return Response(serializer.data)
+      # except Post.DoesNotExist:
+      #       return Response({"detail":"Post is not exist"}, status=status.HTTP_404_NOT_FOUND)
+            
+  
