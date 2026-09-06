2ed155672c273b46677588503681ac538707f737
zia ziaei <rezaziaei28@gmail.com>
2026-09-06 18:41:42 +0330

    Use ModelSerializer and geting arguments from post model

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from blog.models import Post
 
+# class PostSerializers(serializers.Serializer):
+#       id = serializers.IntegerField()
+#       title = serializers.CharField(max_length=255)
 
-class PostSerializers(serializers.Serializer):
-      id = serializers.IntegerField()
-      title = serializers.CharField(max_length=255)
+class PostSerializers(serializers.ModelSerializer):
+
+      class Meta:
+            model = Post
+            fields = ['id', 'author', 'title', 'content', 'status', 'created_date', 'published_date']
