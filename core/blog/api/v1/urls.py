e241334ec09b7dfed261283f82cb706979224f41
zia ziaei <rezaziaei28@gmail.com>
2026-09-13 18:41:30 +0330

    learn DefaultRouter and use them in urls instead of urlpatterns

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
-from .views import *
-
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
 
 app_name = 'api-v1'
 
-urlpatterns = [
-    # This path is about function base view 
-    # path("post/", post_list, name="post-list"),
-    # path("post/<int:id>/", post_detail, name="post-detail")
+router = DefaultRouter()
+router.register('post', PostViewSet, basename='post')
+urlpatterns = router.urls
 
-    # This path is about class base view PostList and PostDetail 
-    # path("post/", PostList.as_view(), name="post-list"),
-    # path("post/<int:id>/", PostDetail.as_view(), name="post-detail")
+# urlpatterns = [
+#     # This path is about function base view 
+#     # path("post/", post_list, name="post-list"),
+#     # path("post/<int:id>/", post_detail, name="post-detail")
 
-    # This path is about viewset
-    path('post/', PostViewSet.as_view({'get':'list','post':'create'}), name='post-list'),
-    path('post/<int:id>/', PostViewSet.as_view({'get':'retrieve','put':'update','delete':'destroy', 'patch':'partial_update'}), name='post-retrieve'),
+#     # This path is about class base view PostList and PostDetail 
+#     # path("post/", PostList.as_view(), name="post-list"),
+#     # path("post/<int:id>/", PostDetail.as_view(), name="post-detail")
 
-]
+#     # This path is about viewset
+#     path('post/', PostViewSet.as_view({'get':'list','post':'create'}), name='post-list'),
+#     path('post/<int:id>/', PostViewSet.as_view({'get':'retrieve','put':'update','delete':'destroy', 'patch':'partial_update'}), name='post-retrieve'),
+
+# ]
