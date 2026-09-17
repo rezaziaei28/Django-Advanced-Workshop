fcf3fcf9f42c0670b2ddec896ff1659332b3bea7
zia ziaei <rezaziaei28@gmail.com>
2026-09-17 12:59:05 +0330

    add absolute_url with SerializerMethodField
    use SerializerMethodField and request.build_absolute_uri in PostSerializers because things that need request should be in serializer not model and things that do not need request can stay in the model so now snippet and relative_url come from model methods and absolute_url comes from the serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,14 +10,21 @@ class PostSerializers(serializers.ModelSerializer):
       snippet = serializers.ReadOnlyField(source='get_snippet')
       relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
 
+      absolute_url = serializers.SerializerMethodField()
+
       # this for adding read only fields
       # author = serializers.ReadOnlyField() this is equal bottom author
       # author = serializers.CharField(read_only=True)
 
       class Meta:
             model = Post
-            fields = ['id', 'author', 'relative_url','snippet', 'title', 'content', 'status', 'created_date', 'published_date']
+            fields = ['id', 'author', 'relative_url', 'absolute_url',
+                      'snippet', 'title', 'content', 'status', 'created_date', 'published_date']
             read_only_fields = ['author',]
+
+      def get_absolute_url(self, object):
+            request = self.context.get('request')
+            return request.build_absolute_uri(object.pk)
 class CategorySerializers(serializers.ModelSerializer):
 
       class Meta:
