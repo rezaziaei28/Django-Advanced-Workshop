df94e7cc604c94fb851d23ad5bede7d93a5a9a15
zia ziaei <rezaziaei28@gmail.com>
2026-09-19 09:11:00 +0330

    use to_representation to show nested category in PostSerializers
    add to_representation and show full category with CategorySerializers and remove snippet with rep.pop and add category to fields

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -4,13 +4,20 @@ from blog.models import Post,Category
 # class PostSerializers(serializers.Serializer):
 #       id = serializers.IntegerField()
 #       title = serializers.CharField(max_length=255)
+class CategorySerializers(serializers.ModelSerializer):
+
+      class Meta:
+            model = Category
+            fields = ['id', 'name']
 
 class PostSerializers(serializers.ModelSerializer):
 
       snippet = serializers.ReadOnlyField(source='get_snippet')
       relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
-
       absolute_url = serializers.SerializerMethodField()
+
+      # category = CategorySerializers()
+      # category = serializers.SlugRelatedField(many=False,slug_field='name', queryset=Category.objects.all())
 
       # this for adding read only fields
       # author = serializers.ReadOnlyField() this is equal bottom author
@@ -19,14 +26,15 @@ class PostSerializers(serializers.ModelSerializer):
       class Meta:
             model = Post
             fields = ['id', 'author', 'relative_url', 'absolute_url',
-                      'snippet', 'title', 'content', 'status', 'created_date', 'published_date']
+                      'snippet', 'title', 'category','content', 'status', 'created_date', 'published_date']
             read_only_fields = ['author',]
 
       def get_absolute_url(self, object):
             request = self.context.get('request')
             return request.build_absolute_uri(object.pk)
-class CategorySerializers(serializers.ModelSerializer):
 
-      class Meta:
-            model = Category
-            fields = ['id', 'name']
+      def to_representation(self, instance):
+            rep = super().to_representation(instance)
+            rep['category'] = CategorySerializers(instance.category).data
+            rep.pop('snippet', None)
+            return rep
