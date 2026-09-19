e849f0e04408afea08d5621f96b7b2a1e341227f
zia ziaei <rezaziaei28@gmail.com>
2026-09-19 13:40:02 +0330

    learn custom permission
    add IsOwnerOrReadOnly permission with has_object_permission to check if the request user is the owner of the post and use it in PostModelViewSet so now only the owner can edit or delete a post

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
@@ -184,7 +185,7 @@ class PostViewSet(viewsets.ViewSet):
 
 class PostModelViewSet(viewsets.ModelViewSet):
       queryset = Post.objects.filter(status=True)
-      permission_classes = [IsAuthenticatedOrReadOnly]  
+      permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]  
       serializer_class = PostSerializers 
 
       @action(methods=['get'],detail=False)
