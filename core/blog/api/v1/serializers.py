632be95b3936667d19aa07311423ac8b5b94c64b
zia ziaei <rezaziaei28@gmail.com>
2026-09-19 12:53:28 +0330

    learn to_representation and create in PostSerializers
    use parser_context kwargs pk to detect list or detail and show different fields for each one and add state field and remove snippet relative_url absolute_url in list and remove content in detail and override create to set author from request user profile instead of input data

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from blog.models import Post,Category
+from accounts.models import Profile, User
 
 # class PostSerializers(serializers.Serializer):
 #       id = serializers.IntegerField()
@@ -25,7 +26,7 @@ class PostSerializers(serializers.ModelSerializer):
 
       class Meta:
             model = Post
-            fields = ['id', 'author', 'relative_url', 'absolute_url',
+            fields = ['id', 'author', 'image','relative_url', 'absolute_url',
                       'snippet', 'title', 'category','content', 'status', 'created_date', 'published_date']
             read_only_fields = ['author',]
 
@@ -34,7 +35,20 @@ class PostSerializers(serializers.ModelSerializer):
             return request.build_absolute_uri(object.pk)
 
       def to_representation(self, instance):
+            request = self.context.get('request')
             rep = super().to_representation(instance)
+            rep['state'] = 'list'
+            # print(request.__dict__)
+            if request.parser_context.get('kwargs').get('pk'):
+                  rep.pop('snippet', None)
+                  rep.pop('relative_url', None)
+                  rep.pop('absolute_url', None)
+            else:
+                  rep.pop('content', None)                
             rep['category'] = CategorySerializers(instance.category).data
-            rep.pop('snippet', None)
             return rep
+
+      def create(self, validated_data):
+            """Attach the current user's profile as the author."""
+            validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+            return super().create(validated_data)
